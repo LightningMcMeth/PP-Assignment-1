db7a50c728a812f51ed4f0505f6d31c336430b8f
LightningMcMeth <mhlibovytskyy@kse.org.ua>
2023-09-20 01:35:08 +0300

    committing what I currently have, before making certain changes.

--- assignment-1-paradigms/assignment-1-paradigms.cpp
+++ assignment-1-paradigms/assignment-1-paradigms.cpp
@@ -1,3 +1,4 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -6,10 +7,6 @@
 //TODO:
 
 /*
-1. Test r/w
-2. Add dynamic 2d array
-3. insertion mechanism
-4. substring search
 https://www.youtube.com/watch?v=wL2FHiBSh2E&ab_channel=UBCECESS
 */
 
@@ -21,20 +18,15 @@ int main() {
     char userInput[100] = "";
     char userText[100] = "";
 
-    char** userTextD = (char**)calloc(1, sizeof(char*));    //rows
-    for (int i = 0; i < 3; i++) //columns 
+    int arrRows = 3;
+    char** userTextD = (char**)calloc(arrRows, sizeof(char*));    //rows
+    for (int i = 0; i < arrRows; i++) //columns 
     {
         userTextD[i] = (char*)calloc(100, sizeof(char));
     }
 
     int textLength = 0;
     int textRow = 0;
-
-    errno_t fileErr = 1;
-    bool endOfName = false;
-    char fileName[100];
-    FILE* file;
-
 
 
     while (1)
@@ -49,35 +41,42 @@ int main() {
             
             printf("Enter text to append:");
             fgets(userInput, sizeof(userInput), stdin);
+            userInput[strlen(userInput) - 1] = '\0';
 
-            /*
             if (textLength + atoi(userInput) <= 100) {
+
                 textLength += strlen(userInput);
 
                 userInput[textLength - 1] = '\0';
 
-                strcat_s(userText, userInput);
+                //strcpy_s(userTextD[textRow], sizeof(userInput), userInput);
+                strcat_s(userTextD[textRow], sizeof(userInput), userInput);
             }
-            else {
-                printf("\ncringe\n");
+            else if (textRow < arrRows - 1 || textLength + atoi(userInput) >= 100){
+
+                textLength = strlen(userInput);
+                textRow += 1;
+
+                //strcpy_s(userTextD[textRow], sizeof(userInput), userInput);
+                strcat_s(userTextD[textRow], sizeof(userInput), userInput);
+
+            }
+            else if (strlen(userInput) >= 100)
+            {
+                printf("reallocate some memory or something.\n");
                 break;
             }
-            */
 
-            strcpy_s(userTextD[textRow], sizeof(userInput), userInput);
+            //strcpy_s(userTextD[textRow], sizeof(userInput), userInput);
 
             printf("\nText appended successfully: ");
-            printf("%s", userTextD[textRow]);
+            printf("%s", userInput);
 
             break;
         case 2:
 
-            for (int i = 0; i < sizeof(userText); ++i) 
-            {
-                printf("%c", userText[i]);
-            }
 
-            for (int i = 0; i < textRow + 1; i++)
+            for (int i = 0; i < arrRows; i++)
             {
                 for (int j = 0; j < 100; j++)
                 {
@@ -89,13 +88,41 @@ int main() {
         case 3:
             
             if (textLength < 100) {
-                userText[textLength - 1] = '\n';
+                userTextD[textRow][textLength - 1] = '\n';
             }
 
             printf("Newline added successfully.\n");
 
             break;
         case 4:
+        {
+            FILE* file;
+            char fileName[25];
+            
+            printf("Enter the file name: ");
+            fgets(fileName, sizeof(fileName), stdin);
+
+            if (strlen(fileName) > 0 && fileName[strlen(fileName) - 1] == '\n') {
+                fileName[strlen(fileName) - 1] = '\0';
+            }
+
+            errno_t fileErr = fopen_s(&file, fileName, "w");
+
+            if (fileErr == 0) {
+                for (int i = 0; i < arrRows; i++)
+                {
+                    fputs(userTextD[textRow], file);
+                    fclose(file);
+                }
+            }
+
+            printf("Text written to file successfully.\n");            
+            break;
+        }
+        case 5:
+        {
+            FILE* file;
+            char fileName[25];
 
             printf("Enter the file name: ");
             fgets(fileName, sizeof(fileName), stdin);
@@ -103,58 +130,39 @@ int main() {
             if (strlen(fileName) > 0 && fileName[strlen(fileName) - 1] == '\n') {
                 fileName[strlen(fileName) - 1] = '\0';
             }
-            /*
+
             printf("Enter the file name: ");
             fgets(userInput, sizeof(userInput), stdin);
 
-            for (int i = 0; i < sizeof(userInput); i++)
-            {
-                if(userInput[i] == '\n') {
-                    endOfName = true;
-                    userInput[i] = '\0';
-                }
-
-                if (endOfName) {
-                    userInput[i] = '\0';
-                }
-            }
-            */
-
-            fileErr = fopen_s(&file, fileName, "w");
-
-            if (fileErr == 0) {
-                fputs(userText, file);
-                fclose(file);
-            }
-
-            printf("Text written to file successfully.\n");
-
-            break;
-        case 5:
-
-            printf("Enter the file name: ");
-            fgets(fileName, sizeof(fileName), stdin);
-
-            if (strlen(fileName) > 0 && fileName[strlen(fileName) - 1] == '\n') {
-                fileName[strlen(fileName) - 1] = '\0';
-            }
-
-            //printf("Enter the file name: ");
-            //fgets(userInput, sizeof(userInput), stdin);
-            
-            fileErr = fopen_s(&file, fileName, "r");
+            errno_t fileErr = fopen_s(&file, fileName, "r");
 
             if (fileErr == 1) {
                 printf("Error opening file, or the file is empty.\n");
                 break;
             }
 
+            /*
             while (fgets(userInput, sizeof(userText), file) != NULL) {
                 printf("%s", userInput);
-            }
-            printf("File read successfully.\n");
+            }*/
 
+            for (int i = 0; i < arrRows; i++)
+            {
+                if (fgets(userInput, sizeof(userText), file) == NULL)
+                {
+                    break;
+                }
+
+                for (int j = 0; j < 100; j++)
+                {
+                    userTextD[i][j] = userInput[j];
+                }
+                printf("%s", userInput);
+            }
+
+            printf("File read successfully.\n");
             break;
+        }
         case 6:
             printf("Command not implemented yet.");
             break;
@@ -162,7 +170,7 @@ int main() {
             printf("Command not implemented yet.");
             break;
         case 8:
-            return 0;
+            system("cls");
             break;
         default:
             printf("I don't know that command.\n");
@@ -193,3 +201,43 @@ int main() {
             //strcat(userTextInput, userInput);
             //arrSize = strlen(userTextInput); //don't know why this is flagged as a warning
             */
+
+            /*
+                        printf("Enter the file name: ");
+                        fgets(userInput, sizeof(userInput), stdin);
+
+                        for (int i = 0; i < sizeof(userInput); i++)
+                        {
+                            if(userInput[i] == '\n') {
+                                endOfName = true;
+                                userInput[i] = '\0';
+                            }
+
+                            if (endOfName) {
+                                userInput[i] = '\0';
+                            }
+                        }
+                        */
+
+
+/*
+{
+            FILE* file;
+
+            //printf("Enter the file name: ");
+            //fgets(fileName, sizeof(fileName), stdin);
+
+            file = fopen("myfile.txt", "w");
+            for (int i = 0; i < textRow; i++)
+            {
+                fputs(userTextD[i], file);
+            }
+
+            if (fgets(userInput, sizeof(userInput), file) != NULL)
+            {
+                printf("%s", userInput);
+            }
+            fclose(file);
+
+            break;
+        }*/
